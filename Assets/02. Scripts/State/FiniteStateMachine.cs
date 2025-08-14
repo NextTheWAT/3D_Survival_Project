54eda66831d7f460d7d4661cb9170b8e13c1e4ae
broccoli1962 <aq0011@naver.com>
2025-08-14 19:55:07 +0900

    [ feat ] StateMachine
    StateMachine 기초 설계 완

---
+++ Assets/02. Scripts/State/FiniteStateMachine.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine
+{
+    private List<IState> _states;
+    private IState _currentState;
+
+    public FiniteStateMachine(List<IState> states)
+    {
+        _states = states;
+    }
+
+    public void Run()
+    {
+        Update();
+    }
+
+    public void ChangeTo(int index)
+    {
+        _currentState.End();
+        _currentState = _states[index];
+        _currentState.Start();
+    }
+
+    public void Update()
+    {
+        _currentState.Update();
+    }
+}
