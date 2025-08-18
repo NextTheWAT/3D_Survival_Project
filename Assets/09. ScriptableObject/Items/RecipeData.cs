1bff2d6438ad949f47ad2e3c305dc4d01c0c8985
Younga <youngajin93@gmail.com>
2025-08-18 22:11:15 +0900

    [feat]RecipeData SO

---
+++ Assets/09. ScriptableObject/Items/RecipeData.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class RecipeIngredient
+{
+    public int itemId;
+    public int amount;
+}
+[CreateAssetMenu(fileName = "RecipeData", menuName = "Database/New Recipe Data")]
+public class RecipeData : ScriptableObject
+{
+    public List<RecipeIngredient> resources;
+    public int outputItemId;
+    public float processTime;
+}
+
