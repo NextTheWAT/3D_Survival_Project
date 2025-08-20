54ed29b8a9a668f8d3bc510ede4f0a49d3e6b2fc
Glory Day <alwaysgloryday@gmail.com>
2025-08-21 05:43:50 +0900

    Fix: Building system troubles
    - Resolving collision detection issue
    - Set transparent color when material(game object) can be built

--- Assets/02. Scripts/Object/BuildingSimulationRenderer.cs
+++ Assets/02. Scripts/Object/BuildingSimulationRenderer.cs
@@ -5,12 +5,20 @@ namespace Object
     [RequireComponent(typeof(CollisionDetector))]
     public class BuildingSimulationRenderer : MonoBehaviour
     {
+        #region CONSTANT FIELD API
+
+        private const string RenderingModePropertyName = "_Mode";
+
+        #endregion
+
         [Header("Renderer Settings")]
         [Range(0f, 1f)]
         [SerializeField] private float alpha = 0.4f;
 
         private Renderer _renderer;
-        private Material _material;
+        private Material[] _materials;
+        private int[] _modes;
+        private Color[] _colors;
 
         private CollisionDetector _detector;
 
@@ -21,26 +29,78 @@ namespace Object
         private void Awake()
         {
             _renderer = GetComponent<Renderer>();
-            _material = _renderer.material;
+            _materials = _renderer.materials;
 
             _detector = GetComponent<CollisionDetector>();
 
+            var length = _materials.Length;
+            _modes = new int[length];
+            _colors = new Color[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                _modes[i] = (int)_materials[i].GetFloat("_Mode");
+                _colors[i] = _materials[i].color;
+            }
+
             _green = new Color(0f, 1f, 0f, alpha);
             _red = new Color(1f, 0f, 0f, alpha);
-            _default = _material.color;
         }
 
-        public void SetSimulationColor(int layerMask)
+        public void SetSimulationColor(bool isEnabled)
         {
-            var rotation = transform.rotation;
-            var color = _detector.IsCollisionDetected(layerMask) ? _red : _green;
+            var color = isEnabled ? _green : _red;
 
-            _material.color = color;
+            for (var i = 0; i < _materials.Length; i++)
+            {
+                _materials[i].color = color;
+
+                SetRenderingMode(_materials[i], RenderingMode.Transparent);
+            }
         }
 
         public void SetDefaultColor()
         {
-            _material.color = _default;
+            for (var i = 0; i < _materials.Length; i++)
+            {
+                _materials[i].color = _colors[i];
+
+                SetRenderingMode(_materials[i], RenderingMode.Opaque);
+            }
         }
+
+        private void SetRenderingMode(Material material, RenderingMode mode)
+        {
+            switch (mode)
+            {
+                case RenderingMode.Opaque:
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                    material.SetInt("_ZWrite", 1);
+                    material.DisableKeyword("_ALPHATEST_ON");
+                    material.DisableKeyword("_ALPHABLEND_ON");
+                    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = -1;
+                    break;
+                case RenderingMode.Transparent:
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    material.SetInt("_ZWrite", 0);
+                    material.DisableKeyword("_ALPHATEST_ON");
+                    material.EnableKeyword("_ALPHABLEND_ON");
+                    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = 3000;
+                    break;
+            }
+        }
+
+        #region ENUMERATION API
+
+        private enum RenderingMode
+        {
+            Opaque, Transparent
+        }
+
+        #endregion
     }
 }
