5f8849c9e6466fd6375f5af56fe12e13a9a4fed1
Glory Day <alwaysgloryday@gmail.com>
2025-08-20 17:46:50 +0900

    Feature: Building System

---
+++ Assets/02. Scripts/Object/BuildingSimulationRenderer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Object
+{
+    [RequireComponent(typeof(CollisionDetector))]
+    public class BuildingSimulationRenderer : MonoBehaviour
+    {
+        [Header("Renderer Settings")]
+        [Range(0f, 1f)]
+        [SerializeField] private float alpha = 0.4f;
+
+        private Renderer _renderer;
+        private Material _material;
+
+        private CollisionDetector _detector;
+
+        private Color _green;
+        private Color _red;
+        private Color _default;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+            _material = _renderer.material;
+
+            _detector = GetComponent<CollisionDetector>();
+
+            _green = new Color(0f, 1f, 0f, alpha);
+            _red = new Color(1f, 0f, 0f, alpha);
+            _default = _material.color;
+        }
+
+        public void SetSimulationColor(int layerMask)
+        {
+            var rotation = transform.rotation;
+            var color = _detector.IsCollisionDetected(layerMask) ? _red : _green;
+
+            _material.color = color;
+        }
+
+        public void SetDefaultColor()
+        {
+            _material.color = _default;
+        }
+    }
+}
