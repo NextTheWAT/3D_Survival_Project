7dafe42c09ee33009acc3eaef07afbe5ff42d694
aq0011 <aq0011223344@gmail.com>
2025-08-21 12:31:06 +0900

    [ feat ] monster spawn trigger
    [ feat ] monster spawn trigger

---
+++ Assets/02. Scripts/Enemy/EnemySpawnTrigger.cs
@@ -0,0 +1,35 @@
+﻿using Object.Character.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnTrigger : MonoBehaviour
+{
+    public EnemySpawnArea owner;
+    public int spawnAreaId;
+
+    public void Init(EnemySpawnArea spawner, int id)
+    {
+        owner = spawner;
+        spawnAreaId = id;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("들어감0");
+        owner.SpawnMonster(spawnAreaId);
+        if (other.CompareTag("Player")) //?
+        {
+            Debug.Log("들어감");
+            owner.SpawnMonster(spawnAreaId);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            owner.DeSpawnMonster(spawnAreaId);
+        }
+    }
+}
