2dc93cf75c7a0363d86c9093a529d15550a53db2
Younga <youngajin93@gmail.com>
2025-08-21 14:00:05 +0900

    [feat] 아이템 장착, 툴에 따라 자원 채취 인터렉션

---
+++ Assets/02. Scripts/Inventory/EquipmentController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform handGrabPoint;
+    private GameObject currentItem;
+    private PlayerInteractionController playerInteractionController;
+
+    public GameObject CurrentItem => currentItem;
+    public Transform HandGrabPoint => handGrabPoint;
+
+    private void Awake()
+    {
+        playerInteractionController = GetComponent<PlayerInteractionController>();
+    }
+    //장착
+    public void Equip(EquipItemData itemData)
+    {
+        if (itemData == null || itemData.inGamePrefab == null) return;
+
+        //기존 장착 아이템 제거
+        if (currentItem != null)
+            Destroy(currentItem);
+
+        //grab point에 붙이기
+        currentItem = Instantiate(itemData.inGamePrefab, handGrabPoint);
+        currentItem.transform.localPosition = Vector3.zero;
+        currentItem.transform.localRotation = Quaternion.identity;
+
+        Rigidbody rb = currentItem.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+            rb.useGravity = false;
+        }
+
+        SetEquipptedItemForInteraction(itemData);
+        Debug.Log($"{itemData.name} equipped");
+    }
+
+    //장착 해제
+    public void Unequip()
+    {
+        if (currentItem != null)
+        {
+            Destroy(currentItem);
+            currentItem = null;
+            SetEquipptedItemForInteraction(null);
+        }
+    }
+
+    private void SetEquipptedItemForInteraction(EquipItemData? equipItemData)
+    {
+        playerInteractionController.SetEquipptedItem(equipItemData);
+    }
+}
